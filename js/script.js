e8ede1410616f57a8ca0272bfdf16ee488c340d8
Alianhakim9 <Alianhakim9@gmail.com>
2020-01-05 08:01:42 -0800

    my-profile

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// navbar mobile
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+// akhir navbar mobile
+
+// page-scroll ( pada saat link di klik)
+$('.page-scroll').on('click', function (e) {
+
+    var tujuan = $(this).attr('href');
+
+    var elemenTujuan = $(tujuan);
+
+    $('html , body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1000, 'easeInOutExpo');
+
+    e.preventDefault();
+});
+
